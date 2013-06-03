8a68a6ae3cf942081927698088a341274c007731
MaxDolgov <lazydays@ya.ru>
2013-06-03 15:56:16 +0400

    complete state support

--- js/quexe.js
+++ js/quexe.js
@@ -1,32 +1,15 @@
-  // dependency: ctor.js
-  function clone(o){
-    var cloned;
-    if ('object' == typeof o) {
-      if (isArray(o)){
-        cloned = [];
-        for (var i = 0, l = o.length; i < l; i++) cloned[i] = ctor.clone(o[i]);
-      } else {
-        cloned = {};
-        for (var p in o) cloned[p] = ctor.clone(o[p]);
-      }
-    } else return o;
-    return cloned;
-  }
   
-  // Function.bind() aka Currying micro implementaion
-  function curry(fn, context){
-    var args = [].slice.call(arguments, 2);
-    return function(){
-      fn.apply(context, args.concat([].slice.call(arguments)))
-    };
-  } 
-  
-  var job = {
+  var qexJob = {
 
-    state: 'defined',
+    state: 'defined', // defined|running|stopped|complete
     
     conf: {
-      maxTicks: 150
+      maxTicks: 75,
+      states: {
+        running:  {},
+        stopped:  { milestone: true },
+        complete: { milestone: true }
+      }  
     },
     
     data: {
@@ -36,15 +19,21 @@
     },
     
     progress: {
-      complete: 0,
       tickHead: 0,
       startTime: 0,
       endTime: 0,
       usedTime: 0
     },
     
+    runnable: function() {
+      return 'complete' !== this.state;
+      // return (-1 === ['complete','running'].indexOf(this.state));
+    },
+    
     run: function(){
-      qex.log('run()');
+      qex.log('run() state:'+this.state);
+      if (!this.runnable()) 
+        return;
       this.state = 'running';
       if (0 === this.progress.tickHead)
         this.progress.startTime = Date.now();
@@ -53,24 +42,38 @@
     },
     
     stop: function( reason ){
-      qex.log('stop( * ' + reason + ' * )');
-      this.state = 'stopped';
-      var now = Date.now();
-      this.progress.endTime = now;
-      this.progress.usedTime += now - this.progress.startTime;
+      return this.setState('stopped', reason);
+    },
+
+    complete: function( reason ) {
+      return this.setState('complete', reason);
+    },
+    
+    setState: function( state, reason ) {
+      qex.log('setState( "' + state + '" over "'+this.state+'", *' + reason + '* )');
+      if (!(state in this.conf.states) || ('complete' === this.state && 'stopped' === state))
+        return;      
+        
+      this.state = state;
+      var stConf = this.conf.states[ state ];
+      if (stConf && stConf.milestone) {
+        var now = Date.now();
+        this.progress.endTime = now;
+        this.progress.usedTime += now - this.progress.startTime;
+      }
     },
     
     tick: function(){
       this.data.currentArgs = [].slice.call(arguments);
       qex.log('tick() #'+this.progress.tickHead+' state: ', this.state); 
-      if ('stopped' == this.state){
+      if (-1 < ['stopped', 'complete'].indexOf(this.state)){
         qex.log(' * usedTime:'+this.progress.usedTime+'ms');
         return;
       }
 
       var self = this;
       self.doStep().afterStep();
-      setTimeout(curry(self.tick, self), 0);
+      setTimeout(ctor.curry(self.tick, self), 0);
 
     },
     
@@ -84,7 +87,7 @@
       this.progress.tickHead++;
       
       if (this.progress.tickHead > this.conf.maxTicks)
-        this.stop('tick limit reached');
+        this.complete('maxTicks amount reached');
         
       return this;
     }
@@ -106,36 +109,44 @@
     },
     echo: function(){
       var dt = new Date(), 
-          stamp = (dt.getSeconds()+'.'+dt.getMilliseconds());
-      console.log(' * '+ stamp +':\n', qex.logs.join('\n'), '\n=======================================' );
+          stamp = (dt.getMinutes()+':'+dt.getSeconds()+'.'+dt.getMilliseconds());
+      console.log(' * ['+ stamp +']', '\n'+qex.logs.join('\n')+'\n', '=======================================' );
     },  
     
     init: function() {
-      this.job = job;
+      this.job = ctor.clone(qexJob);
       this.exec('run from init');
     },
     
     exec: function( reason ){
       qex.log(' * qex.exec( *' + reason + '* )  ');
-      this.job.run();
-      setTimeout(curry(qex.rest, qex), qex.conf.jobTimeout, ['job timeout!']);
+      if (this.job.runnable()) {
+        this.job.run();
+        setTimeout(ctor.curry(qex.rest, qex), qex.conf.jobTimeout, ['job timeout!']);
+      } else {
+        qex.end('exec() job is not runnable');
+      }
     },
     
     rest : function(reason) {
       this.job.stop(reason);
-      qex.nextLeap( curry(qex.exec, qex) );
+      qex.nextLeap(ctor.curry(qex.exec, qex));
     },
     
     // process.nextTick analogue
     nextLeap: function(callback) {
-      qex.log(' * qex.nextLeap() ');
+      qex.log(' * qex.nextLeap( '+(1+qex.conf.leapCount)+' / '+qex.conf.maxLeaps+' ) ');
       
       if (++qex.conf.leapCount < qex.conf.maxLeaps)
         setTimeout(callback, qex.conf.leapTimeout, ['nextLeap timeout!']);
       else 
-        qex.end();
+        qex.end('maxLeaps amount reached');
     },
-    end: function() {
+    
+    end: function( reason ) {
+      qex.log('qex.end( * ' +  reason + ' * )');
+      qex.log(' * usedTime:'+this.job.progress.usedTime+'ms');
+      
       qex.echo();
     }
   }
